a3774af2d6b3d250cf9479d3b68c131d35a8e19a
IrinaGM <irina.golionko@gmail.com>
2020-02-08 16:04:33 -0500

    added all new blog code for gatsby setup

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `Code Kraken`,
+    description: `Let's chat about technology`,
     author: `@gatsbyjs`,
   },
   plugins: [
@@ -12,8 +12,17 @@ module.exports = {
         name: `images`,
         path: `${__dirname}/src/images`,
       },
+    }, 
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `markdowns`,
+        path: `${__dirname}/src/markdown-pages`,
+      },
     },
+    `gatsby-transformer-remark`,
     `gatsby-transformer-sharp`,
+    `gatsby-plugin-styled-components`,
     `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-manifest`,
